daa7b4a8767cf14e15dc9d4c54369781eaa9060b
Creadark <danbodnar28@gmail.com>
2025-01-08 02:35:22 +0300

    4 commit. Сhanged speed calculation

--- ftracker.go
+++ ftracker.go
@@ -49,12 +49,12 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 	switch {
 	case trainingType == "Бег":
 		distance := distance(action)
-		speed := meanSpeed(action, duration) * minInH
+		speed := meanSpeed(action, duration)
 		calories := RunningSpentCalories(action, weight, duration)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Ходьба":
 		distance := distance(action)
-		speed := meanSpeed(action, duration) * minInH
+		speed := meanSpeed(action, duration)
 		calories := WalkingSpentCalories(action, duration, weight, height)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Плавание":
